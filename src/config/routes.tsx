5819c78c489eab4859b06397b07c48ba0851f5de
lw <1109160884@qq.com>
2023-01-07 13:24:12 +0800

    欢迎路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,7 +1,20 @@
+import { RouteRecordRaw } from 'vue-router'
 import { Bar } from '../views/Bar'
 import { Foo } from '../views/Foo'
-
-export const routes = [
+import { Welcome } from '../views/Welcome'
+import { First } from '../components/welcome/First'
+import { Second } from '../components/welcome/Second'
+import { Third } from '../components/welcome/Third'
+import { Forth } from '../components/welcome/Forth'
+export const routes: RouteRecordRaw[] = [
     { path: '/', component: Foo },
     { path: '/about', component: Bar },
+    {
+        path: "/welcome", component: Welcome, children: [
+            { path: "/1", component: First },
+            { path: "/2", component: Second },
+            { path: "/3", component: Third },
+            { path: "/4", component: Forth }
+        ]
+    }
 ]
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import { App } from './App'
 import {routes} from "./config/routes"
 import { createRouter, createWebHashHistory } from 'vue-router'
-
+import { history } from './shared/history'
 
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history,
     routes,
 })
 
