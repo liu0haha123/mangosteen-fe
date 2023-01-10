3f3ba1e73da723271dca94a0f458fc57cb55b263
lw <1109160884@qq.com>
2023-01-10 20:17:59 +0800

    默认路由修改

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -7,10 +7,10 @@ import { Second } from '../components/welcome/Second'
 import { Third } from '../components/welcome/Third'
 import { Forth } from '../components/welcome/Forth'
 export const routes: RouteRecordRaw[] = [
-    { path: '/', component: Foo },
-    { path: '/about', component: Bar },
+    { path: '/', redirect: "/welcome" },
     {
         path: "/welcome", component: Welcome, children: [
+            { path: "", redirect: "/welcome/1" },
             { path: "1", component: First },
             { path: "2", component: Second },
             { path: "3", component: Third },
